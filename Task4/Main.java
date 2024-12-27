4711f7930cb5378166660f5868ef4d2137dd1ecf
maurrogrant <46741805+maurrogrant@users.noreply.github.com>
2024-12-27 13:31:51 +0300

    Create Main.java

---
+++ Task4/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        Person athlete = new Athlete("Иван");
+        Person regularPerson = new RegularPerson("Петр");
+
+        System.out.println("Спортсмен:");
+        athlete.run();
+        athlete.setRunningSpeed(20);
+        athlete.swim();
+        athlete.setSwimmingStyle("Кроль");
+
+        System.out.println("\nОбычный человек:");
+        regularPerson.run();
+        regularPerson.setRunningSpeed(5);
+        regularPerson.swim();
+        regularPerson.setSwimmingStyle("Брасс");
+    }
+}
