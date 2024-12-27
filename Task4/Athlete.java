330c2cceb29f02830bc8e2382f2a7a9ea22c9b82
maurrogrant <46741805+maurrogrant@users.noreply.github.com>
2024-12-27 13:27:20 +0300

    Create Athlete.java

---
+++ Task4/Athlete.java
@@ -0,0 +1,25 @@
+public class Athlete extends Person {
+    public Athlete(String name) {
+        super(name);
+    }
+
+    @Override
+    public void run() {
+        System.out.println(getName() + " бежит быстро.");
+    }
+
+    @Override
+    public void swim() {
+        System.out.println(getName() + " плавает быстро.");
+    }
+
+    @Override
+    public void setRunningSpeed(int speed) {
+        System.out.println(getName() + " бежит со скоростью " + speed + " км/ч.");
+    }
+
+    @Override
+    public void setSwimmingStyle(String style) {
+        System.out.println(getName() + " плавает стилем " + style + ".");
+    }
+}
