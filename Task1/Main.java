25ff7a9df1d583f4268b140b361b983f4d1217e6
maurrogrant <46741805+maurrogrant@users.noreply.github.com>
2024-12-27 13:01:36 +0300

    Create Main.java

---
+++ Task1/Main.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        VendingMachine vendingMachine = new VendingMachine();
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+
+        do {
+            System.out.println("\n1. Посмотреть меню напитков");
+            System.out.println("2. Внести деньги");
+            System.out.println("3. Выбрать напиток");
+            System.out.println("4. Выход");
+            System.out.print("Введите ваш выбор: ");
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    vendingMachine.displayMenu();
+                    break;
+                case 2:
+                    System.out.print("Введите сумму для внесения: ");
+                    double amount = scanner.nextDouble();
+                    vendingMachine.addMoney(amount);
+                    break;
+                case 3:
+                    System.out.print("Введите номер напитка: ");
+                    int drinkNumber = scanner.nextInt();
+                    vendingMachine.selectDrink(drinkNumber);
+                    break;
+                case 4:
+                    System.out.println("Выход из программы.");
+                    break;
+                default:
+                    System.out.println("Неправильный выбор. Пожалуйста, попробуйте снова.");
+            }
+        } while (choice != 4);
+
+        scanner.close();
+    }
+}
