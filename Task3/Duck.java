d9b35eb58c8feb63206a87a361b4540e4cbb93ef
maurrogrant <46741805+maurrogrant@users.noreply.github.com>
2024-12-27 13:12:29 +0300

    Create Duck.java

---
+++ Task3/Duck.java
@@ -0,0 +1,21 @@
+public class Duck extends Animal implements Fly, Swim, Run {
+    @Override
+    public String getName() {
+        return "Утка";
+    }
+
+    @Override
+    public void fly() {
+        System.out.println(getName() + " летает.");
+    }
+
+    @Override
+    public void swim() {
+        System.out.println(getName() + " плавает.");
+    }
+
+    @Override
+    public void run() {
+        System.out.println(getName() + " бегает.");
+    }
+}
