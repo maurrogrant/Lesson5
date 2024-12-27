db22e7e31d8fa29ae0596a93ceb368a911989491
maurrogrant <46741805+maurrogrant@users.noreply.github.com>
2024-12-27 13:11:14 +0300

    Create Dog.java

---
+++ Task3/Dog.java
@@ -0,0 +1,11 @@
+public class Dog extends Animal implements Run {
+    @Override
+    public String getName() {
+        return "Собака";
+    }
+
+    @Override
+    public void run() {
+        System.out.println(getName() + " бегает.");
+    }
+}
