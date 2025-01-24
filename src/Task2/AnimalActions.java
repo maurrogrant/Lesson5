d3b894670592cdf9bb853fa909450a4525c65065
maurrogrant <46741805+maurrogrant@users.noreply.github.com>
2025-01-24 14:43:28 +0300

    Create AnimalActions.java

---
+++ src/Task2/AnimalActions.java
@@ -0,0 +1,8 @@
+package Task2;
+
+// Интерфейс для действий животных
+public interface AnimalActions {
+    void fly(); // Метод для полета
+    void run(); // Метод для бега
+    void swim(); // Метод для плавания
+}
